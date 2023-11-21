b258da8cc7b1c7e4c107f7468d76b3d5d11b4fa5
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-11-21 14:43:37 -0500

    url actualizada

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
    *
    *    http://example.com/
    */
-  public string $baseURL = 'http://localhost/framework_ci4/public/';
+  public string $baseURL = 'http://localhost/CRUD_Codeigniter4/public/';
 
   /**
    * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
