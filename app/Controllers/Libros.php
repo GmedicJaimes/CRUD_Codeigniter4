9a06a3d69d850a062ebe6fdad1c050a6449b19eb
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-11-28 17:36:37 -0500

    identificacion de cada linea de codigo

--- app/Controllers/Libros.php
+++ app/Controllers/Libros.php
@@ -7,26 +7,35 @@ use App\Models\Libro;
 
 class Libros extends Controller
 {
-
   //? Creacion del metodo
   public function index()
   {
-    //? consultar la informacion
+    //consultar la informacion
+    //se crea una nueva instancia de la clase libro
     $libro = new Libro();
+
+    //se consulta la base de datos, para obtener todos los libros con el metodo findAll(), ordenados por el campo id, de forma ascendente y se almacena en el array $datos con la clave 'libros'
     $datos['libros'] = $libro->orderBy('id', 'ASC')->findAll();
 
+    //se agregan dos vistas, al array $datos, con las claves cabecera y pie. 
     $datos['cabecera'] = view('template/cabecera');
     $datos['pie'] = view('template/piepagina');
 
+    //retorna una vista, llamda libros/listar y le paso el array $datos
     return view('libros/listar', $datos);
   }
 
 
   public function crear()
   {
+    //se crea un array vacio llamado $datos
+    //agrego una vista llamada cabecera al array $datos, con la clave cabecera
     $datos['cabecera'] = view('template/cabecera');
+    //agrego una vista llamada piepagina al array $datos, con la clave pie
     $datos['pie'] = view('template/piepagina');
 
+
+    //devuelvo la vista libros/crear y le paso el array $datos
     return view('libros/crear', $datos);
   }
 
@@ -36,16 +45,20 @@ class Libros extends Controller
 
     //Realizo un if para preguntar si la imagen existe
     if ($imagen = $this->request->getFile('imagen')) {
-      //creo una nueva variable, y guardo el valor de imagen
+      //creo una nueva variable, y le indico que me cree un nombre random al momento de guardarla
       $nuevoNombre = $imagen->getRandomName();
+      //indico, que al momento de guardar la imagen, la mueva a esa direccion
       $imagen->move('../public/uploads/', $nuevoNombre);
 
+      //array de valores guardados por el form
       $datos = [
+        //guardo en cada variable, el valor que recibo del form, de su input correspondiente
         'nombre' => $this->request->getVar('nombre'),
         'autor' => $this->request->getVar('autor'),
         'imagen' => $nuevoNombre
       ];
 
+      //inserto la informacion del form, en mi tabla libro de mi base de datos
       $libro->insert($datos);
     }
 
--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -6,8 +6,11 @@ use CodeIgniter\Model;
 
 class Libro extends Model
 {
+  //? $table y $primarykey son utilizados por todos los metodos CRUD
   protected $table      = 'libros';
   // Uncomment below if you want add primary key
   protected $primaryKey = 'id';
+
+  //los nombres que esten dentro del array $allowedFields, seran los unicos que se podran manipular a través del modelo.
   protected $allowedFields = ['nombre', 'imagen', 'autor'];
 }
